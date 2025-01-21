2dfa361bfef4b2fae1dd6d721d1fcf3321fb477e
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-22 02:22:50 +0530

    add clerk for authentication and user management features; update header and home page layout

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -1,4 +1,15 @@
 import clsx from "clsx";
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 type HeaderProps = {
   className?: string;
@@ -14,7 +25,29 @@ export default function Header({ className, style }: HeaderProps) {
       )}
       style={style}
     >
-      Header
+      <div className="lg:max-w-screen-lg mx-auto flex items-center justify-between h-full">
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image src="/mascot.svg" alt="mascot" width={40} height={40} />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+        <ClerkLoading>
+          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn >
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <Button size="lg" variant="ghost">
+                Login
+              </Button>
+            </SignInButton>
+          </SignedOut>
+        </ClerkLoaded>
+      </div>
     </div>
   );
 }
