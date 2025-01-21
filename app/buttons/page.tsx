612d01f80dec2f1313490c224e034cb266255036
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-21 21:22:51 +0530

    add buttons page and update button styles;modify layout metadata and gitignore

---
+++ app/buttons/page.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@/components/ui/button";
+
+export default function ButtonsPage() {
+  return (
+    // <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+    <div className="p-4 flex flex-wrap gap-4">
+      <Button>Default</Button>
+      <Button variant="primary">Primary</Button>
+      <Button variant="primaryOutline">Primary Outline</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="secondaryOutline">Secondary Outline</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="dangerOutline">Danger Outline</Button>
+      <Button variant="super">Super</Button>
+      <Button variant="superOutline">Super Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+
+      <Button variant="sidebar">Sidebard</Button>
+      <Button variant="sidebarOutline">Sidebard Outline</Button>
+    </div>
+  );
+}
