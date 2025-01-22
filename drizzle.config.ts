8b65aaed0806bb9cd4dd923a1d2f955f386b783d
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-22 21:00:32 +0530

    feat: add database configuration and schema for courses; include dotenv and drizzle-orm dependencies

---
+++ drizzle.config.ts
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
+
+// export default {
+//   schema: "./db/schema.ts",
+//   out: "./drizzle",
+//   driver: "pg",
+//   dbCredentials: {
+//     connectionString: process.env.DATABASE_URL!,
+//   },
+// } satisfies Config;
+
+export default {
+  out: "./drizzle",
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+} satisfies Config;
