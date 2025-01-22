0e158dfc5346f609e1f5e49261a1d7850be57d8e
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-22 18:00:09 +0530

    feat: implement LearnPage layout with Header, FeedWrapper, and UserProgress components; add StickyWrapper for user progress display

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -21,7 +21,7 @@ export default function MainLayout({
         className={cn("lg:pl-[256px] h-full pt-[50px] lg:pt-0", className)}
         style={style}
       >
-        <div className="bg-red-500 h-full">{children}</div>
+        <div className="max-w-[1056px] mx-auto pt-6 h-full">{children}</div>
       </main>
     </>
   );
---
+++ components/user-progress.tsx
@@ -0,0 +1,74 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
+type UserProgressProps = {
+  className?: string;
+  style?: React.CSSProperties;
+  activeCourse: {
+    title: string;
+    imageSrc: string;
+  }; //TODO: replace with DB type
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+};
+
+export default function UserProgress({
+  className,
+  style,
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: UserProgressProps) {
+  return (
+    <div
+      className={cn("flex items-center justify-between gap-x-2", className)}
+      style={style}
+    >
+      <Link href="/course">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            width={32}
+            height={32}
+            className="rounded-md border"
+          />
+          User Progress
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            alt="Points"
+            width={28}
+            height={28}
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            alt="Heart"
+            width={22}
+            height={22}
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="h-4 w-4 stroke-[3]" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  );
+}
