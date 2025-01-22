125569241a1dbba78233dea3baffde88ff389fcd
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-22 16:00:02 +0530

    feat: add SidebarItem component and update Sidebar with navigation links and user authentication

---
+++ components/sidebar-item.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type SideBarItemProps = {
+  className?: string;
+  style?: React.CSSProperties;
+  label: string;
+  iconSrc: string;
+  href: string;
+};
+
+export default function SidebarItem({
+  className,
+  style,
+  label,
+  iconSrc,
+  href,
+}: SideBarItemProps) {
+  const pathname = usePathname();
+  const active = pathname === href;
+
+  return (
+    <div className={cn("", className)} style={style}>
+      <Button
+        variant={active ? "sidebarOutline" : "sidebar"}
+        className="justify-start h-[52px] w-full"
+        asChild
+      >
+        <Link href={href}>
+          <Image
+            src={iconSrc}
+            width={32}
+            height={32}
+            alt={label}
+            className="mr-5"
+          />
+          {label}
+        </Link>
+      </Button>
+    </div>
+  );
+}
