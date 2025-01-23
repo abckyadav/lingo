f155d97cb03fa4d0a5f97a1ae6c0e9bdb9ce8c0b
Abhishek Kumar <anu250867anu@gmail.com>
2025-01-23 21:47:07 +0530

    feat: add user progress retrieval and course relations; implement user progress schema

--- db/queries.ts
+++ db/queries.ts
@@ -1,11 +1,39 @@
 import { cache } from "react";
 import db from "./drizzle";
-import { asc } from "drizzle-orm";
-import { courses } from "./schema";
+import { asc, eq } from "drizzle-orm";
+import { courses, userProgress } from "./schema";
+import { auth } from "@clerk/nextjs/server";
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany({
     orderBy: [asc(courses.id)],
+  });
+
+  return data;
+});
+
+export const getUserProgress = cache(async () => {
+  const { userId } = await auth();
+  console.log("userId:", userId);
+
+  if (!userId) {
+    return null;
+  }
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    //TODO: populate units and lessons
   });
 
   return data;
