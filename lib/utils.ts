52f353043753d794bb8da1b9d80acdcaf29952f1
abckyadav <anu250867anu@gmail.com>
2025-01-30 12:38:11 +0530

    refactor: replace utility constants with centralized constants; update imports

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,11 +4,3 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export const defaultHearts = 5;
-export const pointsPerChallenege = 10;
-
-export enum CHALLENGE_TYPE_ENUM {
-  SELECT = "SELECT",
-  ASSIST = "ASSIST",
-}
