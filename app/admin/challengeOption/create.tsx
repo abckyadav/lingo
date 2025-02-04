aad618e8e497cc2824806ba58af880c62e4a1638
abckyadav <anu250867anu@gmail.com>
2025-02-04 22:15:23 +0530

    feat: add CRUD functionality for challenge options with admin access control

---
+++ app/admin/challengeOption/create.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import {
+  BooleanInput,
+  Create,
+  NumberInput,
+  ReferenceInput,
+  required,
+  SelectInput,
+  SimpleForm,
+  TextInput,
+} from "react-admin";
+
+type ChallengeOptionCreateProps = {
+  className?: string;
+  style?: React.CSSProperties;
+};
+
+export default function ChallengeOptionCreate({
+  className,
+  style,
+}: ChallengeOptionCreateProps) {
+  return (
+    <div className={cn("", className)} style={style}>
+      <Create>
+        <SimpleForm>
+          <TextInput source="text" label="Text" validate={[required()]} />
+          <BooleanInput source="correct" label="Correct Option" />
+
+          <ReferenceInput source="challengeId" reference="challenges" />
+
+          <TextInput source="imageSrc" label="Image URL" />
+
+          <TextInput source="audioSrc" label="Audio URL" />
+        </SimpleForm>
+      </Create>
+    </div>
+  );
+}
